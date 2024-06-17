5eb479b785b44ab3b6453bc099d4db25a057078e
RLUCAS <lucas.reboulet@ynov.com>
2024-06-17 17:08:40 +0200

    create table of database

--- src/database/createDatabase.php
+++ src/database/createDatabase.php
@@ -18,3 +18,38 @@ $createTableUser = ("CREATE TABLE IF NOT EXISTS
     CONSTRAINT unique_User_Email UNIQUE (`Email`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = latin1");
 $dsn->exec($createTableUser);
+
+$createTablePost = ("CREATE TABLE IF NOT EXISTS
+`Post` (
+    `IdPost` int(11) NOT NULL AUTO_INCREMENT,
+    `TitlePost` varchar(255) DEFAULT NULL,
+    `ContentPost` TEXT DEFAULT NULL,
+    `DatePost` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `PicturePost` LONGBLOB DEFAULT NULL,
+    PRIMARY KEY (`IdPost`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = latin1");
+$dsn->exec($createTablePost);
+
+$createTableUserPost = ("CREATE TABLE IF NOT EXISTS
+`UserPost` (
+    `Id`int(11) NOT NULL AUTO_INCREMENT,
+    `IdUser` int(11) DEFAULT NULL,
+    `IdPost` int(11) DEFAULT NULL,
+    PRIMARY KEY (`Id`),
+    CONSTRAINT fk_User_UserPost FOREIGN KEY (`IdUser`) REFERENCES User (`IdUser`),
+    CONSTRAINT fk_Post_UserPost FOREIGN KEY (`IdPost`) REFERENCES Post (`IdPost`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = latin1");
+$dsn->exec($createTableUserPost);
+
+$createTableComment = ("CREATE TABLE IF NOT EXISTS
+`Comment` (
+    `IdComment`int(11) NOT NULL AUTO_INCREMENT,
+    `ContentComment` TEXT DEFAULT NULL,
+    `DateComment` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `IdUser` int(11) DEFAULT NULL,
+    `IdPost` int(11) DEFAULT NULL,
+    PRIMARY KEY (`IdComment`),
+    CONSTRAINT fk_User_Comment FOREIGN KEY (`IdUser`) REFERENCES User (`IdUser`),
+    CONSTRAINT fk_Post_Comment FOREIGN KEY (`IdPost`) REFERENCES Post (`IdPost`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = latin1");
+$dsn->exec($createTableComment);
