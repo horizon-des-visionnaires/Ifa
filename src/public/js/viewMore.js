a59f3f05cc4e62e6dc6ba5c8feea389eccf277ac
RLUCAS <lucas.reboulet@ynov.com>
2024-07-01 13:49:05 +0200

    update button voir plus

--- src/public/js/viewMore.js
+++ src/public/js/viewMore.js
@@ -1,8 +1,12 @@
-function toggleVisibility(elementId) {
+function toggleVisibility(elementId, buttonId) {
     var element = document.getElementById(elementId);
+    var button = document.getElementById(buttonId);
+
     if (element.style.display === "none") {
         element.style.display = "block";
+        button.textContent = "Voir moins";
     } else {
         element.style.display = "none";
+        button.textContent = "Voir plus";
     }
 }
