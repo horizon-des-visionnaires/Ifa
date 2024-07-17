f12592704106cbeb402c0735e32e3bc91655a762
RLUCAS <lucas.reboulet@ynov.com>
2024-07-17 13:11:28 +0200

    add front button + form requestPassPro

--- src/public/js/modalUser.js
+++ src/public/js/modalUser.js
@@ -17,3 +17,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var modal = document.getElementById("ProModal");
+    var btn = document.getElementById("openModalButtonPro");
+    var span = document.getElementsByClassName("close2")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
