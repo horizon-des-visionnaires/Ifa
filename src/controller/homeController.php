46de1efffec680950c9d90a09e97d48d3c2ee07b
RLUCAS <lucas.reboulet@ynov.com>
2024-07-18 15:15:42 +0200

    display 5 random user pro on page home

--- src/controller/homeController.php
+++ src/controller/homeController.php
@@ -1,25 +1,25 @@
 <?php
 
-namespace home; // Déclare le namespace pour ce fichier.
+namespace home;
 
-use Twig\Environment; // Importe la classe Environment de Twig pour gérer l'environnement de templates.
-use Twig\Loader\FilesystemLoader; // Importe la classe FilesystemLoader de Twig pour charger les templates à partir du système de fichiers.
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-require 'vendor/autoload.php'; // Charge automatiquement les classes installées via Composer.
+require 'vendor/autoload.php'; 
 
-require_once __DIR__ . '/../model/homeModel.php'; // Inclut le fichier contenant la classe homeModel.
+require_once __DIR__ . '/../model/homeModel.php';
 
 class homeController
 {
-    protected $twig; // Propriété pour l'objet Twig.
-    private $loader; // Propriété pour le loader de Twig.
-    private $homeModel; // Propriété pour l'objet homeModel.
+    protected $twig;
+    private $loader;
+    private $homeModel;
 
     public function __construct()
     {
-        $this->loader = new FilesystemLoader(__DIR__ . '/../views/templates'); // Initialise le loader de Twig avec le répertoire des templates.
-        $this->twig = new Environment($this->loader); // Initialise l'environnement Twig avec le loader.
-        $this->homeModel = new \home\homeModel(); // Crée une instance de la classe homeModel.
+        $this->loader = new FilesystemLoader(__DIR__ . '/../views/templates');
+        $this->twig = new Environment($this->loader);
+        $this->homeModel = new \home\homeModel();
     }
 
     public function home()
@@ -39,11 +39,13 @@ class homeController
         }
 
         $this->logOut();
+        $userProData = $this->homeModel->get5UserProRandom();
 
         echo $this->twig->render('home/home.html.twig', [
             'isConnected' => $isConnected,
             'userId' => $userId,
             'IsAdmin' => $IsAdmin,
+            'userProData' => $userProData
         ]);
     }
 
--- src/model/homeModel.php
+++ src/model/homeModel.php
@@ -21,4 +21,34 @@ class homeModel
         $this->dsn = new PDO("mysql:host=mysql;dbname=ifa_database", "ifa_user", "ifa_password");
         $this->dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
+
+    public function get5UserProRandom()
+    {
+        try {
+            $stmt = $this->dsn->query("
+                SELECT 
+                    IdUser,
+                    FirstName, 
+                    LastName, 
+                    ProfilPicture, 
+                    ProfilDescription
+                FROM User
+                WHERE IsPro = 1
+                ORDER BY RAND()
+                LIMIT 5
+            ");
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($results as &$row) {
+                if (!is_null($row['ProfilPicture'])) {
+                    $row['ProfilPicture'] = base64_encode($row['ProfilPicture']);
+                }
+            }
+
+            return $results;
+        } catch (PDOException $e) {
+            $error = "error: " . $e->getMessage();
+            echo $error;
+        }
+    }
 }
